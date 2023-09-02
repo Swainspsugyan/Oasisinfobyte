9d17e6af79d2782fb9ec8766d10705217576bcdb
Swainspsugyan <134998946+Swainspsugyan@users.noreply.github.com>
2023-09-02 23:44:08 +0530

    Update GuessTheNumber.java

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,68 +1,53 @@
-import java.util.Scanner;
 import java.util.Random;
-
-class FirstGame {
-    int counter = 0;
-    int myNum;
-    int compNum;
-
-    public FirstGame() {
-        compNum = (int) (Math.random() * 100);
-
-    }
-
-    public void setNumber(int n) {
-        myNum = n;
-    }
-
-    public int isCorrect() {
-        if (myNum == compNum) {
-            return 0;
-        } else if (myNum > compNum) {
-            return -1;
-        } else {
-            return 1;
-        }
-    }
-
-    public void setCounter() {
-        counter++;
-
-    }
-
-    public void getCounter() {
-
-        System.out.println("The number of guesses were " + counter);
-    }
-
-}
+import java.util.Scanner;
 
 public class GuessTheNumber {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        FirstGame obj = new FirstGame();
-        Scanner sc = new Scanner(System.in);
+        int minRange = 1;
+        int maxRange = 100;
+        int maxAttempts = 5;
+        int totalRounds = 3;
+        int totalScore = 0;
 
-        while (true) {
-            System.out.println("Enter a number between 1 to 100 : ");
-            int n = sc.nextInt();
-            obj.setNumber(n);
+        System.out.println("Welcome to Guess the Number!");
+        System.out.println("You have " + totalRounds + " rounds to play.");
 
-            if (obj.isCorrect() == 0) {
-                obj.setCounter();
-                System.out.println("Congratulations !! You have guessed the correct number ");
-                obj.getCounter();
-                break;
-            } else if (obj.isCorrect() == -1) {
-                obj.setCounter();
-                System.out.println("Enter the smaller number");
-            } else if (obj.isCorrect() == 1) {
-                obj.setCounter();
-                System.out.println("Enter the larger number ");
+        for (int round = 1; round <= totalRounds; round++) {
+            int targetNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            int attempts = 0;
+
+            System.out.println("\nRound " + round + " - Guess a number between " + minRange + " and " + maxRange + ".");
+
+            while (attempts < maxAttempts) {
+                System.out.print("Attempt " + (attempts + 1) + "/" + maxAttempts + ": Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess < minRange || userGuess > maxRange) {
+                    System.out.println("Please enter a number within the valid range.");
+                } else if (userGuess == targetNumber) {
+                    int roundScore = maxAttempts - attempts + 1;
+                    totalScore += roundScore;
+                    System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                    System.out.println("Round Score: " + roundScore);
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Try a higher number.");
+                } else {
+                    System.out.println("Try a lower number.");
+                }
+
+                if (attempts == maxAttempts) {
+                    System.out.println("Out of attempts. The correct number was " + targetNumber + ".");
+                }
             }
         }
 
+        System.out.println("\nGame Over!");
+        System.out.println("Total Score: " + totalScore);
+        scanner.close();
     }
-
 }
